e2f82f5ca5ed18cf1d41e049fbd304b888a8500d
Steve Wortham <steve.wortham@wwt.com>
2023-04-26 14:38:36 -0400

    return actual food json from go

--- go/main.go
+++ go/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"encoding/json"
+	"strconv"
 	"time"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
 
-func Index(ctx *fasthttp.RequestCtx) {
-	ctx.WriteString("Welcome!")
+func GetFood(ctx *fasthttp.RequestCtx) {
+	idString := ctx.UserValue("id").(string)
+	id, _ := strconv.Atoi(idString)
+	food := foodMap[id]
+
+	json.NewEncoder(ctx.Response.BodyWriter()).Encode(food)
 }
 
 func Hello(ctx *fasthttp.RequestCtx) {
@@ -23,7 +28,7 @@ func main() {
 	load()
 
 	r := router.New()
-	r.GET("/", Index)
+	r.GET("/food/{id}", GetFood)
 	r.GET("/hello/{name}", Hello)
 
 	log.Fatal(fasthttp.ListenAndServe(":3000", r.Handler))
@@ -32,21 +37,29 @@ func main() {
 func timer(name string) func() {
 	start := time.Now()
 	return func() {
-			fmt.Printf("%s took %v\n", name, time.Since(start))
+		fmt.Printf("%s took %v\n", name, time.Since(start))
 	}
 }
+
+var foodMap map[int]Food
 
 func load() {
 	defer timer("load")()
 	content, err := os.ReadFile("./data/food-data.json")
 	if err != nil {
-			log.Fatal("Error when opening file: ", err)
+		log.Fatal("Error when opening file: ", err)
 	}
 
 	// Now let's unmarshall the data into `payload`
 	var payload FoundationFoods
 	err = json.Unmarshal(content, &payload)
 	if err != nil {
-			log.Fatal("Error during Unmarshal(): ", err)
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	foodMap = make(map[int]Food)
+
+	for _, food := range payload.FoundationFoods {
+		foodMap[food.FdcID] = food
 	}
 }
