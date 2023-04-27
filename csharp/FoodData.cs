7c2c33d3f82a22ad4bb5d5f417bdcde987778f68
Steve Wortham <steve@bucketsoft.com>
2023-04-27 11:47:41 -0400

    search by nutrients

--- csharp/FoodData.cs
+++ csharp/FoodData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class FoodData
 {
@@ -28,7 +29,7 @@ public class FoodData
 		}
 	}
 
-	public async Task load()
+	public async Task Load()
 	{
 		var foundationFoods = await JsonFileReader.ReadAsync<FoundationFood>(@"data/food-data.json");
 		var foods = foundationFoods.FoundationFoods;
@@ -57,5 +58,78 @@ public class FoodData
 		{
 			this.nutrientMap[key] = this.nutrientMap[key].OrderBy(f => f.nutrientValue).ToList();
 		}
+	}
+	
+	public IEnumerable<int> FilterByNutrient(int nutrientId, float minValue, float maxValue)
+	{
+		this.nutrientMap.TryGetValue(nutrientId, out var nutrients);
+
+		if (nutrients == null)
+		{
+			return Array.Empty<int>();
+		}
+
+		var minIndex = FindMinIndex(nutrients, minValue);
+		var maxIndex = FindMaxIndex(nutrients, maxValue, minIndex);
+
+		var matchingNutrientAndFoods = nutrients.GetRange(minIndex, maxIndex - minIndex + 1);
+		return matchingNutrientAndFoods.Select(f => f.fdcId);
+	}
+
+	private int FindMinIndex(List<NutrientAndFood> nutrients, float value)
+	{
+		var low = 0;
+		var high = nutrients.Count;
+		var mid = 0;
+		var minIndex = mid;
+
+		while (low <= high)
+		{
+			mid = (int)Math.Floor((high + low) / 2d);
+
+			if (nutrients[mid].nutrientValue < value)
+			{
+				low = mid + 1;
+			}
+			else if (nutrients[mid].nutrientValue > value)
+			{
+				high = mid - 1;
+				minIndex = mid;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+
+		return minIndex;
+	}
+
+	private int FindMaxIndex(List<NutrientAndFood> nutrients, float value, int minIndex)
+	{
+		var low = minIndex;
+		var high = nutrients.Count - 1;
+		var mid = 0;
+		var maxIndex = mid;
+
+		while (low <= high)
+		{
+			mid = (int)Math.Floor((high + low) / 2d);
+			if (nutrients[mid].nutrientValue < value)
+			{
+				low = mid + 1;
+				maxIndex = mid;
+			}
+			else if (nutrients[mid].nutrientValue > value)
+			{
+				high = mid - 1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+
+		return maxIndex;
 	}
 }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting.Server;
 
 var foodData = new FoodData();
-await foodData.load();
+await foodData.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -16,6 +16,17 @@ app.MapGet("/food/{id}", (int id) => {
 	}
 
 	return Results.Ok(food);
+});
+
+app.MapGet("/search/nutrients/{nutrients}", (string nutrients) =>
+{
+	var nutrientIdAndRange = nutrients.Split(":");
+	var nutrientId = int.Parse(nutrientIdAndRange[0]);
+	var minMax = nutrientIdAndRange[1].Split("-");
+	var minValue = int.Parse(minMax[0]);
+	var maxValue = int.Parse(minMax[1]);
+	var matchingFdcIds = foodData.FilterByNutrient(nutrientId, minValue, maxValue);
+	return Results.Ok(matchingFdcIds);
 });
 	
 app.Run();
