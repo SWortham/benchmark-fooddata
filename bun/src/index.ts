766fa3b6fb2e9a2390fc4a7795b4fda0b79f532e
Steve Wortham <steve.wortham@wwt.com>
2023-04-28 16:27:32 -0400

    removed Hono for bun and used bun directly

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,4 +1,3 @@
-import { Hono } from 'hono'
 import { FoodData } from './food-data';
 
 const foodData = new FoodData();
@@ -6,42 +5,65 @@ console.time('Loaded data');
 foodData.load();
 console.timeEnd("Loaded data");
 
-const app = new Hono();
+const FOOD_BASE_URL = "/food/";
+const SEARCH_BASE_URL = "/search/nutrients/";
 
-
-// Parameters:
-// id = fdcId for food
-// Response:
-// complete json of food
-app.get('/food/:id', (c) => {
-  const id = c.req.param("id");
+function getFood(path: string) {
+  const url = path.replace(FOOD_BASE_URL, "");
+  const id = url;
   const fdcId = parseInt(id);
   const food = foodData.foodMap.get(fdcId);
 
   if (!food) {
-    return c.notFound();
+    return new Response(undefined, {status: 404});
   }
 
-  return c.json(food);
-});
+  return new Response(JSON.stringify(food));
+}
 
-// Parameters: 
-// nutrients = id1:minValue-maxValue,id2:minValue-maxValue
-// Response:
-// array of fdcId of each matching food
-app.get('/search/nutrients/:nutrients', (c) => {
-  const nutrientFilter = c.req.param("nutrients");
-
+function getSearchByNutrient(path: string) {
+  const nutrientFilter = path.replace(SEARCH_BASE_URL, "");
   const nutrientIdAndRange = nutrientFilter.split(":");
   const nutrientId = parseInt(nutrientIdAndRange[0]);
   const [minValueString, maxValueString] = nutrientIdAndRange[1].split("-");
   const minValue = parseFloat(minValueString);
   const maxValue = parseFloat(maxValueString);    
   const matchingFdcIds = foodData.filterByNutrient(nutrientId, minValue, maxValue);
-  return c.json(matchingFdcIds);
-})
-
-export default {
-  port: 3000,
-  fetch: app.fetch,
+  return new Response(JSON.stringify(matchingFdcIds));
 }
+
+
+Bun.serve({
+  fetch(req: Request) {
+    const url = new URL(req.url);
+    const path = url.pathname;
+
+    if (path.startsWith(FOOD_BASE_URL)) {
+      return getFood(path);
+    }
+    else if (path.startsWith(SEARCH_BASE_URL)) {
+      return getSearchByNutrient(path);
+    }
+
+    return new Response(undefined, {status: 404});
+  },
+
+    // this is called when fetch() throws or rejects
+  // error(err: Error) {
+  //   return new Response("uh oh! :(\n" + err.toString(), { status: 500 });
+  // },
+
+  // this boolean enables bun's default error handler
+  development: false,
+  // note: this isn't node, but for compatibility bun supports process.env + more stuff in process
+
+  // SSL is enabled if these two are set
+  // certFile: './cert.pem',
+  // keyFile: './key.pem',
+
+  port: 3000, // number or string
+
+  hostname: '0.0.0.0',
+
+  baseURI: 'http://0.0.0.0:3000'
+});
