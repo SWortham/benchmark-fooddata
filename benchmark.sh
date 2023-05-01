4534643fcfd945ea75c23b4ddcd0cda10e11afa3
Steve Wortham <steve.wortham@wwt.com>
2023-05-01 16:21:19 -0400

    refined benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,25 @@
+URL1=http://127.0.0.1:3000/food/321358
+URL2=http://127.0.0.1:3000/search/nutrients/1005:0.1-0.1
+URL3=http://127.0.0.1:3000/search/nutrients/1005:0.5-200.0
+MAX_QPS=10000
+MAX_CONNECTIONS=50
+
 # Warmup phase
-curl -I http://127.0.0.1:3000/food/331358
-curl -I http://127.0.0.1:3000/search/nutrients/1005:0.5-200.0
+curl -I $URL1
+curl -I $URL2
+curl -I $URL3
+
+echo "RUNNING BENCHMARKS..."
 
 # Run the benchmark against 3 URLs
-oha -z 2s -c 100 -q 50000 --latency-correction --disable-keepalive http://127.0.0.1:3000/food/321358
-oha -z 2s -c 100 -q 50000 --latency-correction --disable-keepalive http://127.0.0.1:3000/search/nutrients/1005:0.1-0.1
-oha -z 2s -c 100 -q 50000 --latency-correction --disable-keepalive http://127.0.0.1:3000/search/nutrients/1005:0.5-200.0
+echo ""
+echo "$URL1 ..................."
+oha -z 2s -c $MAX_CONNECTIONS -q $MAX_QPS --latency-correction --disable-keepalive $URL1
+
+echo ""
+echo "$URL2 ..................."
+oha -z 2s -c $MAX_CONNECTIONS -q $MAX_QPS --latency-correction --disable-keepalive $URL2
+
+echo ""
+echo "$URL3 ..................."
+oha -z 2s -c $MAX_CONNECTIONS -q $MAX_QPS --latency-correction --disable-keepalive $URL3
